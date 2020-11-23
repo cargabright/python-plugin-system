6bcaffe2a488a342e5b2f9c9478851dfd6b6afbc
Carl Argabright <carl.argabright@casne.com>
2020-11-22 20:37:03 -0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import importlib
+from pathlib import Path
+import sys
+import toml
+# pyinstaller --onefile main.py -n PluginSystem
+
+from utils.plugin_utils import get_plugins
+
+if __name__ == '__main__':
+    print('Plugin System starting...')
+
+    # Get app config
+    config = toml.load('config.toml')
+
+    # Get plugin path and append it to the system path
+    plugin_path = Path(config.get('plugin_path'))
+    sys.path.append(str(plugin_path))
+
+    print(f"Loading plugins from: {plugin_path}")
+    plugins = get_plugins(plugin_path)
+
+
+    for p in plugins:
+        # Import plugin
+        print(f"Loading plugin: {p}")
+        plugin_module = importlib.import_module(p, ".")
+        print(f"Imported module: {plugin_module}")
+        plugin = plugin_module.Plugin("Hello", "World", option1=1, option2="Options are good")
+        plugin.execute()
+
+    print("Program finished")
---
+++ plugins/one.py
@@ -0,0 +1,11 @@
+"""
+Plugin #1
+"""
+
+class Plugin:
+    def __init__(self, *args, **kwargs):
+        print("Plugin one initialised")
+        print("Plugin one args: ", args, kwargs)
+
+    def execute(self):
+        print("Executing plugin one")
